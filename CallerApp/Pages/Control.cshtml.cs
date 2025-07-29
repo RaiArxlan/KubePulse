92d172dea26cf5d85118ea689f545eb771fd5471
Rai Arslan <RaiArxlan@gmail.com>
2025-07-29 15:26:10 +0500

    Added functionality to pause/resume and send bathes of request

--- CallerApp/Pages/Control.cshtml.cs
+++ CallerApp/Pages/Control.cshtml.cs
@@ -1,31 +1,39 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-namespace CallerApp.Pages
+
+namespace CallerApp.Pages;
+
+public class ControlModel : PageModel
 {
-	public class ControlModel : PageModel
-	{
-		private readonly RequestWorker _worker;
+    private readonly RequestWorker _worker;
 
-		[BindProperty]
-		public int Interval { get; set; } = 5;
+    [BindProperty] public int Interval { get; set; } = 5;
+    [BindProperty] public int BurstCount { get; set; } = 1;
+    [BindProperty] public string ServiceUrl { get; set; } = "http://processorapi:8080/process";
 
-		[BindProperty]
-		public string ServiceUrl { get; set; } = "http://processorapi:8080/process";
+    public bool IsRunningSuccessfully => _worker.IsRunningSuccessfully();
+    public bool IsPaused => _worker.IsPaused();
 
-		[BindProperty]
-		public bool IsRunningSuccessfully { get; private set; } = false;
+    public ControlModel(RequestWorker worker)
+    {
+        _worker = worker;
+    }
 
-		public ControlModel(RequestWorker worker) => _worker = worker;
-		public void OnPost()
-		{
-			_worker.SetInterval(Interval);
-			_worker.SetUrl(ServiceUrl);
-			IsRunningSuccessfully = _worker.IsRunningSuccessfully();
-		}
-
-		public void OnGet()
-		{
-			IsRunningSuccessfully = _worker.IsRunningSuccessfully();
-		}
-	}
+    public void OnPost()
+    {
+        if (Request.Form.ContainsKey("start"))
+        {
+            _worker.Resume();
+        }
+        else if (Request.Form.ContainsKey("stop"))
+        {
+            _worker.Pause();
+        }
+        else
+        {
+            _worker.SetInterval(Interval);
+            _worker.SetBurst(BurstCount);
+            _worker.SetUrl(ServiceUrl);
+        }
+    }
 }
--- CallerApp/RequestWorker.cs
+++ CallerApp/RequestWorker.cs
@@ -4,28 +4,62 @@ public class RequestWorker : BackgroundService
 {
     private readonly IHttpClientFactory _factory;
     private int _interval = 5;
+    private int _burstCount = 1;
     private string _url = "http://processorapi:8080/process";
     private bool _isRunning = false;
+    private volatile bool _paused = false;
+
     public RequestWorker(IHttpClientFactory factory) => _factory = factory;
-    public void SetInterval(int sec) => _interval = sec;
-    public void SetUrl(string url) => _url = url;
-    public bool IsRunningSuccessfully() => _isRunning;
+
+    public void SetInterval(int sec) => _interval = Math.Max(1, sec);
+    public void SetBurst(int count) => _burstCount = Math.Max(1, count);
+    public void SetUrl(string url) => _url = url?.Trim();
+    public void Pause() => _paused = true;
+    public void Resume() => _paused = false;
+    public bool IsRunningSuccessfully() => _isRunning && !_paused;
+    public bool IsPaused() => _paused;
+
     protected override async Task ExecuteAsync(CancellationToken token)
     {
         var client = _factory.CreateClient();
+
         while (!token.IsCancellationRequested)
         {
+            if (_paused)
+            {
+                await Task.Delay(1000, token);
+                continue;
+            }
+
+            var tasks = new List<Task>();
+            for (int i = 0; i < _burstCount; i++)
+                tasks.Add(SendRequest(client));
+
             try
             {
-                await client.GetAsync(_url);
+                await Task.WhenAll(tasks);
                 _isRunning = true;
             }
-            catch (Exception ex)
+            catch
             {
-                Console.WriteLine($"Error occurred: {ex.Message}");
                 _isRunning = false;
             }
+
             await Task.Delay(_interval * 1000, token);
+        }
+    }
+
+    private async Task SendRequest(HttpClient client)
+    {
+        try
+        {
+            var response = await client.GetAsync(_url);
+            response.EnsureSuccessStatusCode();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error occurred during burst request: {ex.Message}");
+            throw;
         }
     }
 }
